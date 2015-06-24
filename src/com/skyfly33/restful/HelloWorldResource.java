d57f649fea29359b759fffdce3989391e322228c
skyfly33 <imfly7@naver.com>
2015-06-24 17:59:35 +0900

    @Path annotation에 정규표현식 적용 예제 추가.

--- src/com/skyfly33/restful/HelloWorldResource.java
+++ src/com/skyfly33/restful/HelloWorldResource.java
@@ -1,16 +1,18 @@
 package com.skyfly33.restful;
 
-import javax.ws.rs.Path;
 import javax.ws.rs.GET;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
-@Path("helloworld")
+@Path("/helloworld")
 public class HelloWorldResource {
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String greeting() {
-		return "Hello,World!!";
+	@Path("{name : ([a-zA-Z])*}")
+	public String greeting(@PathParam("name") String name) {
+		return "Hello, " + name;
 	}
 }
