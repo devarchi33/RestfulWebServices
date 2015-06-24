ee49c84578390e6e5fdfb82c3c6977de38a57eb2
LG <LG@LG-PC>
2015-06-25 08:19:26 +0900

    @Media.type annotation add.

--- src/com/skyfly33/restful/RestEasyExample.java
+++ src/com/skyfly33/restful/RestEasyExample.java
@@ -3,12 +3,15 @@ package com.skyfly33.restful;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/publish")
 public class RestEasyExample {
 
 	@GET
+	@Produces(MediaType.TEXT_PLAIN)
 	@Path("/{message}")
 	public Response publishMessage(@PathParam("message") String msgStr) {
 		String responseStr = "Received message : " + msgStr;
